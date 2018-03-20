2a9b9517097bb15d5276b47ae554a892b583ad5b
Fernando Tsuda <ftsuda.senac@gmail.com>
2018-03-19 21:57:16 -0300

    Versão inicial Spring MVC + exemplos get

---
+++ exemplos-spring/src/test/java/br/senac/tads4/dsw/exemplosspring/ExemplosSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package br.senac.tads4.dsw.exemplosspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExemplosSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
